c292cce6dd6c613a2e0a7b844c9f8d8bbf956cbe
Morgan Fudge <mfudge92@gmail.com>
2018-03-12 01:48:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(y, c, m) {
+  this.year = y;
+  this.color = c;
+  this.model = m;
+}
+
+function Driver(y, c, m) {
+  this.name = y;
+  this.age = c;
+  this.experience = m;
+}
+
+function PickupLocation(y, c, m) {
+  this.address = y;
+  this.city = c;
+}
