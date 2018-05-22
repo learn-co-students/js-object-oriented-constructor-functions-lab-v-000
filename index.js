634a8720d4ff6087834c1fec61e88a16d0147d19
Alex <github email address>
2018-05-22 17:58:40 -0400

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ const PickupLocation = function PickupLocation(address,city){
       this.city = city
 
 }
+
+
+//solution written as followed
+
+
+//    function Scooter(year, color, model){
+//      this.year = year;
+//      this.color = color;
+//      this.model = model
+//    }
+//    
+//    function Driver(name, age, experience){
+//      this.name = name;
+//      this.age = age;
+//      this.experience = experience;
+//    }
+//    
+//    function PickupLocation(address, city){
+//      this.address = address;
+//      this.city = city;
+//    }
