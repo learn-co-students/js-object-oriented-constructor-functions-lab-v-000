b8761404b8f49c6b870696f3f1c1e2935adf2e36
sgibson47 <sgibson47@gmail.com>
2018-10-07 13:41:29 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+// Constructor Functions!
+
+// function MyConstructor(key1, key2, ...){
+//   this.key1 = key1
+//   this.key2 = key2
+//   ...
+// }
+
+// Scooter with year, color, and model properties
+function Scooter(year, color, model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+// Driver with name, age, and experience properties
+function Driver(name, age, experience){
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+// PickupLocation with address and city properties
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city
+}
+
